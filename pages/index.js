2e20ff083813efa141469341bd4df597f1ef298f
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-02-16 13:05:02 -0300

    add dashboard page

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
 import styled from 'styled-components'
 import { useWeb3 } from '@3rdweb/hooks'
+import { Dashboard } from './Dashboard'
 
 export default function Home() {
-  const  { connectWallet } = useWeb3()
+  const { address , connectWallet } = useWeb3()
   return (
-   <Wrapper>
-     <WalletConnect>
-      <Button onClick={() => connectWallet('injected')}>
-        Connect Wallet
-      </Button>
-      <Details>
-        You need Chrome to be
-        <br/> able to run this app. 
+    <Wrapper>
+      {address ? (
+        <Dashboard address={address} />
+      ) : (
+          <WalletConnect>
+            <Button onClick={() => connectWallet('injected')}>
+              Connect Wallet
+            </Button>
+            <Details>
+              You need Chrome to be
+        <br /> able to run this app.
       </Details>
-     </WalletConnect>
-   </Wrapper>
+          </WalletConnect>
+        )}
+
+    </Wrapper>
   )
 }
 
