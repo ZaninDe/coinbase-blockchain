a870f16f1aef632cb073ccdfdb3f55cbd79f14bf
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-02-21 19:55:48 -0300

    styled properties CoinItem added

--- components/modal/CoinItem.js
+++ components/modal/CoinItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const CoinItem = ({
   token,
@@ -16,3 +17,51 @@ const CoinItem = ({
 
 export default CoinItem
 
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 1rem 0.5rem;
+  border-radius: 0.5rem;
+  margin-bottom: 0.3rem;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #0e0f14;
+  }
+`
+
+const Main = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+
+const Icon = styled.div`
+  margin-right: 1rem;
+  height: 1.8rem;
+  width: 1.8rem;
+  border-radius: 50%;
+  overflow: hidden;
+  display: grid;
+  place-items: center;
+
+  & > img {
+    height: 120%;
+    width: 120%;
+    object-fit: cover;
+  }
+`
+
+const NameDetails = styled.div``
+
+const Name = styled.div`
+  font-size: 1.1rem;
+  margin-bottom: 0.2rem;
+`
+
+const Balance = styled.div``
+
+const IsSelected = styled.div`
+  margin-left: 0.5rem;
+  color: #3773f4;
+`
